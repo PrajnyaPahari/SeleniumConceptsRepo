967ef34882e80cae10da76f6a049d67125f654f3
PrajnyaPahari <pragya.pahari.44@gmail.com>
2024-03-21 00:32:19 -0700

    21.03.24_3rd

--- src/com/allianz/frame/FrameDemo1.java
+++ src/com/allianz/frame/FrameDemo1.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FrameDemo1 {
@@ -13,13 +14,14 @@ public class FrameDemo1 {
 
 		String url = "https://netbanking.hdfcbank.com/netbanking";
 		WebDriver driver = null;
-		driver = new EdgeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get(url);
 		driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='login_page']")));
 		driver.findElement(By.name("fldLoginUserId")).sendKeys("Jack123");
 		driver.findElement(By.linkText("CONTINUE")).click();
+		driver.switchTo().defaultContent();
 		
 	}
 
