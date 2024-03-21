447c4b2a78c65d264279f38dc7ac82fbd050c521
PrajnyaPahari <pragya.pahari.44@gmail.com>
2024-03-21 00:22:45 -0700

    21_03_2024

--- src/com/allianz/alertBasics/AlertDemo1.java
+++ src/com/allianz/alertBasics/AlertDemo1.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AlertDemo1 {
 
@@ -13,12 +16,15 @@ public class AlertDemo1 {
 		// TODO Auto-generated method stub
 		String url = "https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm";
 		WebDriver driver = null;
-		driver = new ChromeDriver();
+		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get(url);
 		
 		driver.findElement(By.xpath("//img[@alt='Go']")).click();
+		WebDriverWait wait = null;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.alertIsPresent());
 		//driver.findElement(By.xpath("//img[contains"))
 		Alert alert = driver.switchTo().alert();
 		String text = alert.getText();
---
+++ src/com/allianz/frame/FrameDemo1.java
@@ -0,0 +1,26 @@
+package com.allianz.frame;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FrameDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String url = "https://netbanking.hdfcbank.com/netbanking";
+		WebDriver driver = null;
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get(url);
+		driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='login_page']")));
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("Jack123");
+		driver.findElement(By.linkText("CONTINUE")).click();
+		
+	}
+
+}
